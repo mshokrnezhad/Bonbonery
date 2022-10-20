4bec7aea6aca3abc6d09ec54a6da7185c445d658
Masoud <m.shokrnezhad@gmail.com>
2022-10-20 23:48:27 +0300

    the frontend and backend codes are connected.

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,15 @@ const morgan = require("morgan");
 const app = express();
 const api = require("./routers/api");
 
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/v1", api);
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, ".", "public")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, ".", "public", "index.html"));
 });
 
 module.exports = app;
